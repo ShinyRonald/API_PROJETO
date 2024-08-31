ea50ccdf3dacb98937e492dcea318a6f8bbce8a7
Shiny Ronald <shinyronald10@gmail.com>
2024-08-31 19:52:47 -0300

    Subindo API

---
+++ src/main/java/br/com/projeto/API_PROJETO/ApiProjetoApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.API_PROJETO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProjetoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/API_PROJETO/ApiProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.API_PROJETO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
